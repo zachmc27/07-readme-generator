4169bfb30eb8ad8733b548d1778748c6be8aa481
zachmc27 <dzdudes1213@gmail.com>
2025-01-28 23:17:05 -0600

    adding inquirer

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
 import fs from 'fs'
 import generateMarkdown from './utils/generateMarkdown';
+import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
- 
-const questions = [];
+//length: 9
+const questions = ['What is the title of the project?', 'Describe your application', 'List a table of contents for the application:', 'Describe the installation process:', 'Describe the usage:', 'What license does your application use?', 'Who contributed', 'Describe the testing process', 'How can users reach out to ask questions about the application?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -15,7 +16,25 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            }
+        ])
+        .then((response) => {
+            writeToFile('README', generateMarkdown())
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let licenseBadge;
+
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      licenseBadge = 'MIT badge';
+      break;
+    default:
+      licenseBadge = ""
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+let licenseLink;
+
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      licenseLink = 'MIT link';
+      break;
+    default:
+      licenseLink = ""
+
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+let licenseSection;
+
+function renderLicenseSection(license) {
+  switch (license) {
+    case "MIT":
+      licenseSection = 'MIT section';
+      break;
+    default:
+      licenseSection = ""
+
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
