8b82d9676f1565a94e92fbd07829b3014d1d8471
zachmc27 <dzdudes1213@gmail.com>
2025-01-31 17:35:34 -0600

    finished questions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import inquirer from 'inquirer';
 //length: 11  
 const questions = ['What is the title of the project?', 'Describe your application:', 'List prerequisites necessary in an array format:',
      'List installation steps in an array format:', 'List steps to start the application in an array format:', 'Explain how to operate the main functionality of the application:', 
-     'Which license would you like to use?', 'How can a user contribute to the project?', 'Describe the testing process:', 'What is your github username?', 
+     'Which license would you like to use?', 'How can a user contribute to the project?', 'How can a user run tests on the project?', 'What is your github username?', 
      'What email would you like to provide for user questions?'];
 
 // TODO: Create a function to write README file
@@ -57,11 +57,33 @@ function init() {
                 type: 'list',
                 message: questions[6],
                 name: 'license',
-                choices:
+                choices: ['MIT', 'Apache 2.0', 'GPL', 'BSD']
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'contribute'
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'test'
+            },
+            {
+                type: 'input',
+                message: questions[9],
+                name: 'github'
+            },
+            {
+                type: 'input',
+                message: questions[10],
+                name: 'email',
             }
-
         ])
-        .then((response) => {
+        .then((ans) => {
+            const prereqArray = JSON.parse(ans.prerequisites);
+            const installArray = JSON.parse(ans.installation);
+            const startupArray = JSON.parese(ans.startup);
             writeToFile('README.md', generateMarkdown())
         })
 }
